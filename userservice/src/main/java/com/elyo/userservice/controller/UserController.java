9f6d2f85d56f56b79a2c8a2c82cb1017200fa683
elyo9381 <won4413@daum.net>
2023-03-07 14:24:29 +0900

    add config setting

--- userservice/src/main/java/com/elyo/userservice/controller/UserController.java
+++ userservice/src/main/java/com/elyo/userservice/controller/UserController.java
@@ -32,8 +32,16 @@ public class UserController {
 
     @GetMapping("/heath_check")
     public String status(){
-        return String.format("It's Working on port %s" , env.getProperty("local.server.port"));
+
+        return String.format("It's Working on port %s" +
+                ", port(local.server.port) = " , env.getProperty("local.server.port") +
+                ", port(server.port) = " , env.getProperty("server.port") +
+                ", token secret = " , env.getProperty("token.secret") +
+                ", token expiration time = " , env.getProperty("token.expiration_time")
+        );
+
     }
+
 
 
     @GetMapping("/welcome")
